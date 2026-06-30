cc2cb3805e7a6cee5abe85bdd145841936db1ab9
Madan Sharma <frontloopwithmadan@gmail.com>
2026-06-30 23:46:28 +0530

    inital setup

---
+++ script.js
@@ -0,0 +1,33 @@
+let number1 = 12;
+let number2 = 9;
+let number3 = 15;
+let number4 = 18;
+
+
+if (number1 % 2 === 0) {
+    console.log("The number " + number1 + " is even.");
+
+} else {
+    console.log("The number " + number1 + " is odd.");
+}
+
+if (number2 % 2 === 0) {
+    console.log("The number " + number2 + " is even.");
+
+} else {
+    console.log("The number " + number2 + " is odd.");
+}
+
+if (number3 % 2 === 0) {
+    console.log("The number " + number3 + " is even.");
+
+} else {
+    console.log("The number " + number3 + " is odd.");
+}
+
+if (number4 % 2 === 0) {
+    console.log("The number " + number4 + " is even.");
+
+} else {
+    console.log("The number " + number4 + " is odd.");
+}
